5b2abed50a0ba80c1b0c261e6f417daca8ceca22
lcsdovalle <contatolucasthomas@gmail.com>
2024-12-05 11:19:39 -0300

    refact: update settings

--- src/api/settings/base.py
+++ src/api/settings/base.py
@@ -79,7 +79,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": env('REDIS_CONN'),
     }
 }
 
